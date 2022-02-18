4c7b62b8c607f6dde93da8cbf80b5c7a1f0b2dfd
yuanmeng <ke197822@hotmail.com>
2022-02-18 12:50:37 +0800

    spring boot项目初始化

---
+++ zookeeper-server-8001/src/main/java/cn/yam/cloud/ZookeeperServer8001Application.java
@@ -0,0 +1,13 @@
+package cn.yam.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperServer8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperServer8001Application.class, args);
+    }
+
+}
